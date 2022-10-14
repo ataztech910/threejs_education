cd5669a3f103af01edf07a15dbc5dc1d66223e50
Andrei <webconsult.ekb@gmail.com>
2022-10-14 23:07:04 +0200

    save state

--- src/script.js
+++ src/script.js
@@ -88,27 +88,8 @@ const environmentMapTexture = cubeTextureLoader.load([
 // gradientTexture.minFilter = THREE.NearestFilter
 // gradientTexture.magFilter = THREE.NearestFilter
 
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) => {
-        const textGeometry = new TextGeometry(
-            'Andrei Tazetdinov',
-            {
-                font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelOffset: 0,
-                bevelSegments: 5
-            }
-        )
-        const textMaterial = new MeshBasicMaterial()
-        const text = new THREE.Mesh( textGeometry, textMaterial )
-        scene.add(text)
-    }
-)
+
+
 
 // Scene
 const scene = new THREE.Scene()
